793ee12bcfcc2cf5c44183d08a341e306310356b
matakom <mata.komarek@gmail.com>
2024-12-03 12:19:58 +0100

    basic api working

---
+++ src/api/api.ts
@@ -0,0 +1,28 @@
+import Groq from "groq-sdk";
+import systemPrompt from "./systemPrompt.json";
+
+const groq = new Groq({ apiKey: import.meta.env.VITE_AI_API_KEY, dangerouslyAllowBrowser: true });
+
+async function callAPI(userInput: string) {
+
+  try {
+    const completion = await groq.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: systemPrompt["1"],
+        },
+        {
+          role: "user",
+          content: userInput,
+        },
+      ],
+      model: "llama-3.1-70b-versatile", 
+    });
+    console.log(completion.choices[0].message.content);
+  } catch (error) {
+    console.error("Error calling API:", error);
+  }
+}
+
+export default callAPI;
